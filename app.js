cf9c22fe105aabeb29192e47e54c40abd0e4e4be
Ryan Cowie <ryancowie@macbook-pro.attlocal.net>
2018-12-19 13:52:45 -0800

    -m added style to the todo list entries and added a check box

--- app.js
+++ app.js
@@ -10,21 +10,35 @@ $(document).ready(function(){
   const $btnDelete = $('.btn-delete');
   const $btnCLear = $('.btn-clear');
   const $priorityEntry = $('.priority-entry');
-  const $dueDateEntry = $('.due-date-entry');
+  const $startDateEntry = $('.start-date-entry');
   const $taskEntry = $('.task-entry');
   const $whoEntry = $('.who-entry');
-  const $dateDoneEntry = $('.date-done-entry');
+  const $dueDateEntry = $('.due-date-entry');
 
+  for (let i = 0; i < dataInput.length; i++) {
+      var currentData = dataInput[i];
+      var {Priority, startDate, Task, Who, dueDate} = currentData;
+      $listDisplayField.append(`
+        <li class='dataList'>
+        <input type="checkbox">
+        <span class="priority-append">Priority: ${Priority}</span>
+        <span class="start-date-append">Start-Date: ${startDate}</span>
+        <span class="task-append">Task: ${Task}</span>
+        <span class="who-append">Who: ${Who}</span>
+        <span class="due-date-append">Due-Date: ${dueDate}</span>
+        </li>
 
+      `);
+    };
 
   $btnSubmit.on('click', function() {
     if (typeof (Storage) !== 'undefined') {
       let input = {
         Priority: $priorityEntry.val(),
-        dueDate: $dueDateEntry.val(),
+        startDate: $startDateEntry.val(),
         Task: $taskEntry.val(),
         Who: $whoEntry.val(),
-        doneDate: $dateDoneEntry.val()
+        dueDate: $dueDateEntry.val()
       };
       dataInput.push(input);
 
@@ -34,20 +48,6 @@ $(document).ready(function(){
       console.log('todoList', myItemInStorage);
     };
 
-    for (let i = 0; i < dataInput.length; i++) {
-      var currentData = dataInput[i];
-      var {Priority, dueDate, Task, Who, doneDate} = currentData;
-      $listDisplayField.append(`
-        <li>
-        <span>Priority: ${Priority}</span>
-        <span>Due-Date: ${dueDate}</span>
-        <span>Task: ${Task}</span>
-        <span>Who: ${Who}</span>
-        <span>Done-Date: ${doneDate}</span>
-        </li>
-
-      `);
-    };
 
   });
 
