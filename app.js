829bc15b26de4d49ec1fd27349fd450aa5aa435a
Ryan Cowie <ryancowie@macbook-pro.attlocal.net>
2018-12-18 16:41:36 -0800

    -m began implementation of todo list entry fields

--- app.js
+++ app.js
@@ -3,25 +3,34 @@ $(document).ready(function(){
 
   let dataInput = [];
 
-  const $textEntry = $('.text-entry');
   const $listDisplayField = $('.list-display-field');
   const $btnSubmit = $('.btn-submit');
   const $btnDelete = $('.btn-delete');
   const $btnCLear = $('.btn-clear');
+  const $priorityEntry = $('.priority-entry');
+  const $dueDateEntry = $('.due-date-entry');
+  const $taskEntry = $('.task-entry');
+  const $whoEntry = $('.who-entry');
+  const $dateDoneEntry = $('.date-done-entry');
 
 
-  $btnSubmit.on('click', function(){
+
+  $btnSubmit.on('click', function() {
     if (typeof (Storage) !== 'undefined') {
       let input = {
-        textEntry: $textEntry.val()
-      }
+        Priority: $priorityEntry.val(),
+        dueDate: $dueDateEntry.val(),
+        Task: $taskEntry.val(),
+        Who: $whoEntry.val(),
+        doneDate: $dateDoneEntry.val()
+      };
       dataInput.push(input);
 
       localStorage.setItem('todoList', JSON.stringify(dataInput));
-      var myItemInStorage = localStorage.getItem('todoList');
+      var myItemInStorage = JSON.parse(localStorage.getItem('todoList'));
       console.log('todoList', myItemInStorage);
     };
-    
+
     $listDisplayField.text(myItemInStorage);
 
   });
