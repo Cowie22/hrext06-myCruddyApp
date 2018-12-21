8b6c56be09c031cb9449340191148d47a7e4b845
Ryan Cowie <ryancowie@macbook-pro.attlocal.net>
2018-12-20 21:21:24 -0800

    -m added functionality so that completed tasks hold checked box on refresh

--- app.js
+++ app.js
@@ -58,6 +58,7 @@ $(document).ready(function(){
       var currentData = dataInput[i];
       var {Priority, startDate, Task, Who, dueDate, completed} = currentData;
       var newCount = i;
+      var todoChecked = true;
       var listInfo = `
         <li class='dataList'>
         <input type="checkbox">
@@ -69,10 +70,21 @@ $(document).ready(function(){
         <span class="due-date-append">Due-Date: ${dueDate}</span>
         </li>
       `;
+      var completedListInfo = `
+        <li class='dataList'>
+        <input type="checkbox" ${todoChecked ? "checked" : ""}>
+        <span class="list-count">${newCount}</span>
+        <span class="priority-append">Priority: ${Priority}</span>
+        <span class="start-date-append">Start-Date: ${startDate}</span>
+        <span class="task-append">Task: ${Task}</span>
+        <span class="who-append">Who: ${Who}</span>
+        <span class="due-date-append">Due-Date: ${dueDate}</span>
+        </li>
+      `;
       if (!completed) {
         $listDisplayField.append(listInfo);
       } else {
-        $completedDisplayField.append(listInfo);
+        $completedDisplayField.append(completedListInfo);
       }    
     };
 
@@ -93,91 +105,6 @@ $(document).ready(function(){
       var checkedStorage = JSON.parse(localStorage.getItem('todoList'));
       checkedStorage[todoID].completed = false;
       localStorage.setItem('todoList', JSON.stringify(checkedStorage));
-    }
-  });
-
-
-  $btnDelete.on('click', function() {
-    localStorage.removeItem('todoList');
-  });
-
-
-  $btnCLear.on('click', function() {
-    localStorage.clear();
-    $completedDisplayField.remove();
-    $listDisplayField.remove();
-  });
-
-});
-
-
-  $btnSubmit.on('click', function() {
-    if (typeof (Storage) !== 'undefined') {
-      var input = {
-        Priority: $priorityEntry.val(),
-        startDate: $startDateEntry.val(),
-        Task: $taskEntry.val(),
-        Who: $whoEntry.val(),
-        dueDate: $dueDateEntry.val(),
-        completed: false
-      };
-    };
-    var {Priority, startDate, Task, Who, dueDate, completed} = input;
-    var currentSubmit = `
-        <li class='dataList'>
-        <input type="checkbox">
-        <span class="priority-append">Priority: ${Priority}</span>
-        <span class="start-date-append">Start-Date: ${startDate}</span>
-        <span class="task-append">Task: ${Task}</span>
-        <span class="who-append">Who: ${Who}</span>
-        <span class="due-date-append">Due-Date: ${dueDate}</span>
-        </li>
-      `
-    $listDisplayField.append(currentSubmit);
-
-    $('.dataList :checkbox').change(function() {
-    var checkedTodo;
-    if (this.checked) {
-      checkedTodo = $(this).parent();
-      $completedDisplayField.append(checkedTodo);
-      completed = true;
-      //localStorage.removeItem('todoList');
-      //localStorage.setItem('finishedList', JSON.stringify(dataInput));
-    } else {
-      checkedTodo = $(this).parent();
-      completed = false;
-      $listDisplayField.append(checkedTodo);
-      //localStorage.removeItem('finishedList');
-      //localStorage.setItem('todoList', JSON.stringify(dataInput));
-    }
-  });
-
-    dataInput.push(input);
-      if (!input.completed) {
-        localStorage.setItem('todoList', JSON.stringify(dataInput));
-      } else {
-        localStorage.removeItem('todoList');
-        localStorage.setItem('finishedList', JSON.stringify(dataInput));
-      }
-  });
-
-  
-  $('.dataList :checkbox').change(function() {
-    var checkedTodo;
-    if (this.checked) {
-      checkedTodo = $(this).parent();
-      console.log('hello', checkedTodo)
-      $completedDisplayField.append(checkedTodo);
-      console.log(checkedTodo.completed)
-      checkedTodo.completed = true;
-      //localStorage.removeItem('todoList');
-      //localStorage.setItem('finishedList', JSON.stringify(dataInput));
-    } else {
-      checkedTodo = $(this).parent();
-      checkedTodo.completed = false;
-      $listDisplayField.append(checkedTodo);
-      //localStorage.removeItem('finishedList');
-      //localStorage.setItem('todoList', JSON.stringify(dataInput));
     }
   });
 
